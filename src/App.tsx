e3f677f6754d01ceea45bbebc7e75b9626d1adcb
landaverde1196 <alejndro1196@gmail.com>
2020-11-10 11:57:37 -0600

    check answer added

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,23 @@ const App = () => {
     }
   };
 
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameOver) {
+      const answer = e.currentTarget.value;
+
+      const correct = questions[number].correct_answer === answer;
+
+      if (correct) setScore((prev) => prev + 1);
+
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers((prev) => [...prev, answerObject]);
+    }
+  };
 
   const nextQuestion = () => {};
 
